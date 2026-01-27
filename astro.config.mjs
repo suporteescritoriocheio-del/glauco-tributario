8e19fdb9b26895abccaa93e19aa4948965afb84e
Suporte Escritório Cheio <suporteescritoriocheio@gmail.com>
2026-01-27 15:05:49 -0300

    Teste de configuração de conta

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,9 @@ import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
     output: 'hybrid',
-    adapter: vercel(),
+    adapter: vercel({
+        maxDuration: 10,
+    }),
     build: {
         inlineStylesheets: 'auto',
         format: 'file',
